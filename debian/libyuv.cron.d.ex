8cd3ebf925962aa567c5c8b1dbf5e65d3dc50b6b
Rohan Garg <rohan@garg.io>
2018-03-15 17:01:33 +0100

    Drop packaging from source

--- debian/libyuv.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the libyuv package
-#
-0 4	* * *	root	[ -x /usr/bin/libyuv_maintenance ] && /usr/bin/libyuv_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(libyuv):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="libyuv" command="/usr/bin/libyuv"
